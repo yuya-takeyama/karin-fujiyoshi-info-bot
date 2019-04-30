5382e3dfc93420297c0ccf7822c810f8cc7a83e8
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2019-04-30 23:22:38 +0900

    Add rss/news

--- src/handler.ts
+++ src/handler.ts
@@ -101,3 +101,92 @@ export const rssBlog: APIGatewayProxyHandler = async (_event, _context) => {
     };
   }
 };
+
+interface NewsItem {
+  title: string;
+  url: string;
+  date: Date;
+}
+
+const fetchNewsItems = async () => {
+  const page = await fetch(
+    path('/s/k46o/news/list?ima=0000&list%5B%5D=47&reverse=ON'),
+  );
+  const html = await page.text();
+  const dom = new JSDOM(html);
+  const elems = Array.from(
+    dom.window.document.querySelectorAll('.box-news ul li'),
+  );
+
+  const newsItems: NewsItem[] = [];
+  for (const elem of elems) {
+    const dateElem = elem.querySelector('.date');
+    const linkElem = elem.querySelector('.text a');
+
+    if (linkElem && dateElem) {
+      const date = parseFromTimeZone(striptags(dateElem.innerHTML), {
+        timeZone: 'Asia/Tokyo',
+      });
+      const link = linkElem.getAttribute('href');
+      const title = striptags(linkElem.innerHTML);
+
+      if (link && date) {
+        const url = path(link);
+        newsItems.push({
+          title,
+          url,
+          date,
+        });
+      }
+    }
+  }
+
+  return newsItems;
+};
+
+const fetchNewsFeed = async () => {
+  const newsItems = await fetchNewsItems();
+  const feed = new Feed({
+    title: '欅坂46公式サイト 藤吉夏鈴ニュース',
+    description: '「坂道シリーズ」第2弾　欅坂46',
+    feed: path('/'),
+    id: path('/'),
+    link: path('/'),
+    feedLinks: {},
+    copyright: 'Seed & Flower LLC',
+  });
+  for (const newsItem of newsItems) {
+    feed.addItem({
+      title: newsItem.title,
+      link: newsItem.url,
+      date: newsItem.date,
+    });
+  }
+  return feed;
+};
+
+export const rssNews: APIGatewayProxyHandler = async (_event, _context) => {
+  try {
+    const feed = await fetchNewsFeed();
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/atom+xml',
+      },
+      body: feed.atom1(),
+    };
+  } catch (err) {
+    const error = ensureError(err);
+    console.error(error);
+
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        message: error.message,
+      }),
+    };
+  }
+};
